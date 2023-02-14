4b6eb278ed54f3d0808355742d6ad83bd29a63f3
Casey Johnson <caseyjohnsonwv@gmail.com>
2023-02-14 02:23:22 -0500

    make keys and silence actually do things in game

---
+++ games/trackmania.py
@@ -0,0 +1,81 @@
+import ctypes
+import time
+
+UP_ARROW = 0xC8
+DOWN_ARROW = 0xD0
+LIVE_KEYS = set()
+
+class TrackmaniaHandler:
+    def interpret_note(notestring:str) -> None:
+        # notestring is expected to be "{note}{octave}", eg "A5"
+        match notestring:
+            case 'A4':
+                key = DOWN_ARROW
+                LIVE_KEYS.add(key)
+                PressKey(key)
+            case 'A5':
+                key = UP_ARROW
+                LIVE_KEYS.add(key)
+                PressKey(key)
+            case None:
+                # release keys
+                for key in LIVE_KEYS:
+                    ReleaseKey(key)
+                LIVE_KEYS.clear()
+
+
+
+
+### thanks stackoverflow ###
+############################
+
+
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import crepe
 import math
-from pynput.keyboard import Key, Controller
 import sounddevice as sd
 
 # shutting tensorflow up
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# custom note-to-keypress converter
+from games.trackmania import TrackmaniaHandler
 
 
 
@@ -19,30 +21,44 @@ def freq_to_note(frequency):
 
 
 def main():
-    # give python control of my keyboard
-    keyboard = Controller()
-
-    bitrate = 44100
-    duration = 0.2 # seconds
+    bitrate = 16000
+    duration = 0.1 #seconds
     num_frames = int(bitrate * duration)
+
+    # listen for audio and convert to notes
     while True:
         data = sd.rec(num_frames, bitrate, channels=1)
         sd.wait()
-        time, freq, conf, activation = crepe.predict(data, bitrate, model_capacity='tiny', step_size=(duration * 1000 // 10))
+        time, freq, conf, activation = crepe.predict(data, bitrate, model_capacity='tiny', step_size=(duration * 1000 // 50))
         data = zip(freq, conf)
         results = []
         for dataframe in data:
             freq, conf = dataframe
-            # check for frequency audible and confidence 80%
-            if freq > 50 and conf > 0.8:
+            # check for frequency audible and confidence 70%
+            if freq > 50 and conf > 0.7:
                 results.append(freq_to_note(freq))
-        results = list(set(results))
-        for note, octave in results:
-            print(f"{note}{octave}")
-            # sing an F3 into the microphone to kill the terminal
-            if note == 'F' and octave == 3:
-                with(keyboard.pressed(Key.ctrl_l)):
-                    keyboard.tap('c')
+            else:
+                results.append(None)
+
+        # determine most dominant note in burst
+        reduction = {}
+        for res in results:
+            if res is None:
+                ct = reduction.get(None, 0)
+                reduction[None] = ct + 1
+            else:
+                note, octave = res
+                notestring = f"{note}{octave}"
+                ct = reduction.get(notestring, 0)
+                reduction[notestring] = ct + 1
+        print(reduction)
+        reduction = list(reduction.items())
+        reduction.sort(key=lambda item:item[1], reverse=True)
+        dominant_note = reduction[0][0]
+        print(dominant_note)
+
+        # tell trackmania to do something
+        TrackmaniaHandler.interpret_note(dominant_note)
 
 
 
